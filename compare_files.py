4d052bdd373eaa2f0c78cca6a4138e55b7776bdd
Ryan Schlenz <96892298+RyanSchlenz@users.noreply.github.com>
2023-09-19 17:36:06 -0600

    overhaul of compare_files.py
    Changed name to match what it does more accurately. Created a class of file paths instead of 3 separate variables. Added input validation.

--- compare_files.py
+++ compare_files.py
@@ -1,50 +1,70 @@
 import csv
 import json
+import os
+from generate_json_matches import save_common_values_to_json
 
-def find_and_save_matching_entries(csv_file_path, json_file_path, output_file_path):
-    # Initialize a list to store matching entries
-    matching_entries = []
 
-    # Read IP addresses from the CSV file and store them in a set
-    csv_ip_addresses = set()
-    with open(csv_file_path, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            csv_ip_addresses.add(row['ip_address'])
+class EntryMatcher:
+    def __init__(self, csv_file_path, json_file_path, output_file_path):
+        self.csv_file_path = csv_file_path
+        self.json_file_path = json_file_path
+        self.output_file_path = output_file_path
 
-    # Read and process the JSON file
-    with open(json_file_path, 'r') as json_file:
+    def extract_ip_addresses(self, json_str):
         try:
-            json_data = json.load(json_file)
-            
-            # Assuming the JSON data is a list of strings
-            for entry in json_data:
-                try:
-                    entry_dict = json.loads(entry)  # Parse the JSON string into a dictionary
-                    user_data = entry_dict.get('user')
-                    if user_data:
-                        ip_address = user_data.get('ip_address')
-                        if ip_address and ip_address in csv_ip_addresses:
-                            matching_entries.append(entry_dict)
-                except json.JSONDecodeError as e:
-                    print(f"Error decoding JSON: {e}")
+            entry_dict = json.loads(json_str)  # Parse the JSON string into a dictionary
+            user_data = entry_dict.get('user')
+            if user_data:
+                ip_address = user_data.get('ip_address')
+                return ip_address
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
-        except FileNotFoundError:
-            print(f"File not found: {json_file_path}")
+        return None
 
-    # Write the matching entries to a new JSON file
-    if matching_entries:
-        with open(output_file_path, 'w') as output_file:
-            json.dump(matching_entries, output_file, indent=4)
-        print(f"Matching entries saved to {output_file_path}")
-    else:
-        print("No matching entries found.")
+    def find_and_save_matching_entries(self):
+        # Read IP addresses from the CSV file and store them in a set
+        csv_ip_addresses = set()
+        with open(self.csv_file_path, 'r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                csv_ip_addresses.add(row['ip_address'])
+
+        # Read and process the JSON file
+        with open(self.json_file_path, 'r') as json_file:
+            try:
+                json_data = json.load(json_file)
+
+                # Initialize a list to store matching entries
+                matching_entries = []
+
+                for entry_str in json_data:
+                    ip_address = self.extract_ip_addresses(entry_str)
+                    if ip_address and ip_address in csv_ip_addresses:
+                        matching_entries.append(entry_str)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+            except FileNotFoundError:
+                print(f"File not found: {self.json_file_path}")
+
+        # Write the matching entries to a new JSON file
+        if matching_entries:
+            with open(self.output_file_path, 'w') as output_file:
+                output_file.write('\n'.join(matching_entries))
+            print(f"Matching entries saved to {self.output_file_path}")
+        else:
+            print("No matching entries found.")
 
 # Paths to CSV and JSON files
-csv_file_path = 'log.csv'
-json_file_path = 'json_matches.json'
+csv_file_path = '/Users/ryan/Desktop/JLogAnalysis/LogAnalysis/logs/log.csv'
+json_file_path = '/Users/ryan/Desktop/JLogAnalysis/json_matches.json'
 output_file_path = 'csv_json_matches.json'
 
-# Call the function to find and save matching entries
-find_and_save_matching_entries(csv_file_path, json_file_path, output_file_path)
+# Check if the JSON file (json_matches) exists before processing
+if os.path.exists(json_file_path):
+    # Create an instance of EntryMatcher and call the function to find and save matching entries
+    entry_matcher = EntryMatcher(csv_file_path, json_file_path, output_file_path)
+    entry_matcher.find_and_save_matching_entries()
+else:
+    print(f"The file {json_file_path} does not exist.")
+
+# Generate json_matches (you can add your code here to generate json_matches using p_scripts.save_common_values_to_json)
