6c140552feb2d0258c95de81aa2af7f7d4a405b9
Ryan Schlenz <96892298+RyanSchlenz@users.noreply.github.com>
2023-09-19 17:39:01 -0600

    Update main.py
    Updated hard coding issue, added variables, added in the entrymatcher

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from scripts import *
-from compare_files import *
+import generate_csv_matches
+
+log_csv = 'log.csv'
+json_matches = 'json_matches.json'
+csv_matches = 'csv_json_matches.json'
 
 def main():
-   log_file_paths = ['LogAnalysis/logs/log_a.json', 'LogAnalysis/logs/log_b.json']
+    # Define input and output file paths here
+    csv_file_path = log_csv
+    json_file_path = json_matches
+    output_file_path = csv_matches
 
-save_common_values_to_json(log_file_paths, output_file_path)
-
-find_and_save_matching_entries(csv_file_path, json_file_path, output_file_path)
+    # Call the function to compare JSON and CSV data
+    entry_matcher = generate_csv_matches.EntryMatcher(csv_file_path, json_file_path, output_file_path)
+    entry_matcher.find_and_save_matching_entries()
 
 if __name__ == "__main__":
     main()
-    
-    
