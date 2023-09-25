2718421c4c6a5702be9b2f83689a887c72ad6c62
Ryan Schlenz <96892298+RyanSchlenz@users.noreply.github.com>
2023-09-24 18:09:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import generate_csv_matches
-import time
-import schedule
+import datetime
+import logging
+import config
 
-log_csv = 'log.csv'
-json_matches = 'json_matches.json'
-csv_matches = 'csv_json_matches.json'
+log_csv = config.CSV_FILE_PATH
+json_matches = config.JSON_FILE_PATH
+csv_matches = config.OUTPUT_FILE_PATH
 
 def main():
+    logging.basicConfig(filename='log_file_name.log',level=logging.INFO)
     # Define input and output file paths here
     csv_file_path = log_csv
     json_file_path = json_matches
@@ -15,13 +17,9 @@ def main():
     # Call the function to compare JSON and CSV data
     entry_matcher = generate_csv_matches.EntryMatcher(csv_file_path, json_file_path, output_file_path)
     entry_matcher.find_and_save_matching_entries()
-    
-    # Print a message indicating that the script ran successfully
-    print("Script ran successfully!")
 
-# Schedule the main() function to run every 30 minutes
-schedule.every(30).minutes.do(main)
+   # Print a message indicating that the script ran successfully
+    logging.info(f"Success! {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
-while True:
-    schedule.run_pending()
-    time.sleep
+if __name__ == "__main__":
+    main()
