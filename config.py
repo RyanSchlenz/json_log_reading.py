1fed654d4840d28356d5c2b0e5ee2ef073a267da
Ryan Schlenz <96892298+RyanSchlenz@users.noreply.github.com>
2023-09-24 18:10:43 -0600

    Create config.py
    Replace your file paths with the placeholders to use the code.

---
+++ config.py
@@ -0,0 +1,5 @@
+LOG_FILE_PATHS = ['LogAnalysis/logs/log_a.json', 'LogAnalysis/logs/log_b.json']
+CSV_FILE_PATH = 'Desktop/JLogAnalysis/LogAnalysis/logs/log.csv'
+JSON_FILE_PATH = 'Desktop/JLogAnalysis/json_matches.json'
+OUTPUT_FILE_PATH = 'csv_json_matches.json'
+
